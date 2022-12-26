aa86fd2e3ad3895a0252740ff82e6f47dc835ff6
Koroliuk <koroliuk.yaroslav@gmail.com>
2022-12-26 23:36:24 +0200

    Refactore RadarAntennaScene

--- Assets/Scripts/Radar/RadarIndicator/RotatingRadarSignalController.cs
+++ Assets/Scripts/Radar/RadarIndicator/RotatingRadarSignalController.cs
@@ -1,6 +1,6 @@
 using UnityEngine;
 
-namespace RadarIndicator
+namespace Radar.RadarIndicator
 {
     public class RotatingRadarSignalController : MonoBehaviour
     {
--- Assets/Scripts/Radar/RadarSignal/RadarCollisionScript.cs
+++ Assets/Scripts/Radar/RadarSignal/RadarCollisionScript.cs
@@ -1,6 +1,5 @@
 using System.Collections;
 using System.Collections.Generic;
-using RadarIndicator;
 using UnityEngine;
 
 public class RadarCollisionScript : MonoBehaviour
