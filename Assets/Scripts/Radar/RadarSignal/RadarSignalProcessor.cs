eb6a89c5c520f2619755da326035720640dcc2f2
Koroliuk <koroliuk.yaroslav@gmail.com>
2022-12-28 02:15:10 +0200

    Add size adjusting for planes on indicator

--- Assets/Scripts/Radar/RadarSignal/RadarSignalProcessor.cs
+++ Assets/Scripts/Radar/RadarSignal/RadarSignalProcessor.cs
@@ -10,6 +10,7 @@ namespace Radar.RadarSignal
         [SerializeField] private GameObject wayLinePrefab;
 
         private static readonly IDictionary<int, GameObject> WayLines = new Dictionary<int, GameObject>();
+        private static readonly IDictionary<int, float> IconSize = new Dictionary<int, float>();
         private GameObject _targetIcons;
         private GameObject _passiveObstaclePartIcons;
         private GameObject _wayLines;
@@ -37,7 +38,7 @@ namespace Radar.RadarSignal
             var obj = other.gameObject;
             if (obj.layer == LayerMask.NameToLayer("Aircraft"))
             {
-                var position = CreateIcon(obj, targetIconPrefab, _targetIcons);
+                var position = CreateTargetIcon(obj, targetIconPrefab, _targetIcons);
                 position.y += 2;
                 var objId = obj.GetInstanceID();
                 if (WayLines.ContainsKey(objId))
@@ -54,7 +55,7 @@ namespace Radar.RadarSignal
             }
             else if (obj.layer == LayerMask.NameToLayer("Clouds"))
             {
-                CreateIcon(obj, passiveObstaclePartIconPrefab, _passiveObstaclePartIcons);
+                CreateCloudIcon(obj, passiveObstaclePartIconPrefab, _passiveObstaclePartIcons);
             }
         }
 
@@ -70,7 +71,38 @@ namespace Radar.RadarSignal
             lineRenderer.SetPosition(lineRenderer.positionCount - 1, new Vector3(position.x, position.y, position.z));
         }
 
-        private static Vector3 CreateIcon(GameObject obj, GameObject iconPrefab, GameObject parent)
+        private static Vector3 CreateTargetIcon(GameObject obj, GameObject iconPrefab, GameObject parent)
+        {
+            var targetId = obj.GetInstanceID();
+            var position = obj.transform.position;
+            CartesianToPolar(position.x, position.z, out var radius, out var angle);
+            var adjustedRadius = AdjustRadius(radius);
+
+            var icon = Instantiate(iconPrefab, parent.transform, true);
+            var startPosition = new Vector3(adjustedRadius, 3f, 0f);
+            var endPosition = Quaternion.Euler(0f, -angle, 0f) * startPosition;
+            icon.transform.position = endPosition;
+            icon.transform.rotation = obj.transform.rotation;
+
+            float z;
+            if (IconSize.ContainsKey(targetId))
+            {
+                z = IconSize[targetId];
+            }
+            else
+            {
+                var size = obj.GetComponent<MeshRenderer>().bounds.size;
+                var adjustedSize = Mathf.Max(Mathf.Max(size.x, size.z)/40, 0.5f);
+                IconSize.Add(targetId, adjustedSize);
+                z = adjustedSize;
+            }
+            var currScale = icon.transform.localScale;
+            icon.transform.localScale =  new Vector3(currScale.x, currScale.y, z);
+            
+            return endPosition;
+        }
+        
+        private static void CreateCloudIcon(GameObject obj, GameObject iconPrefab, GameObject parent)
         {
             var position = obj.transform.position;
             CartesianToPolar(position.x, position.z, out var radius, out var angle);
@@ -80,7 +112,6 @@ namespace Radar.RadarSignal
             var startPosition = new Vector3(adjustedRadius, 3f, 0f);
             var endPosition = Quaternion.Euler(0f, -angle, 0f) * startPosition;
             icon.transform.position = endPosition;
-            return endPosition;
         }
 
         private static float AdjustRadius(float r)
